0f191a5224738584171bdccc687a735d29d4f94e
ipav-FS <ipavlovsky@gmail.com>
2021-04-04 16:30:03 -0300

    Optimizacion, correcciones de home.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/login.html'));
+    res.sendFile(path.join(__dirname, './views/home.html'));
 });
 
 app.listen(port, () => {
