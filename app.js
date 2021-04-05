19e5a3d934dc0a27376b4ea85c5ae0126d9bf889
ipav-FS <ipavlovsky@gmail.com>
2021-04-05 19:22:00 -0300

    añadido elementos clase 5/4

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const puerto = process.env.PORT ; 
 const path = require('path');
 console.log('Escuchando')
 app.use(express.static('public'));
@@ -10,6 +11,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views', 'login.html'));
 });
 
-app.listen(port, () => {
-    console.log('Example app listening on port '+ port);
+app.listen(puerto || 3000, function() {
+    console.log("Servidor corriendo en el puerto 3000");
 });
