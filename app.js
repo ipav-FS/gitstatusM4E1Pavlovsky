f1ee59870a6e998de86abc69a448635742258f4c
ipav-FS <ipavlovsky@gmail.com>
2021-04-05 19:34:52 -0300

    cambios main

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views', 'login.html'));
+    res.sendFile(path.join(__dirname, '/views', 'home.html'));
 });
 
 app.listen(puerto || 3000, function() {
